74ec4b6026bda183290158560eab4e514003c6e4
evatxu <635619257@qq.com>
2022-10-22 22:28:20 +0800

    feat: test

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,9 +2,33 @@ import { View, Text } from "@tarojs/components";
 import "./index.less";
 
 const Index = () => {
+  const items = [
+    {
+      value: 1,
+      label: '自提'
+    },
+    {
+      value: 2,
+      label: '外卖'
+    },
+    {
+      value: 3,
+      label: '注册会员'
+    },
+    {
+      value: 4,
+      label: '个人资料'
+    },
+  ]
   return (
     <View className='w-10_12 m-auto'>
-      <Text>Hello world!</Text>
+      <View className='flex'>
+        {
+          items.map(item => (<View className='bg-red-300 px-3' key={item.value}>
+            <Text>{item.label}</Text>
+          </View>))
+        }
+      </View>
     </View>
   );
 };
